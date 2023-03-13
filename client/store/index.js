ec3f865d7551ae434a7b9e554f6ff24644c59f69
Aidan Denahy <denahyaidan@gmail.com>
2023-03-13 19:58:44 -0400

    storeUpdate

--- client/store/index.js
+++ client/store/index.js
@@ -1,12 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { combineReducers } from "redux";
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import axios from "axios";
+import MembersReducer from "./MembersReducer";
+import StudentsReducer from "./StudentsReducer";
+import loggingMiddleware from "redux-logger"; // https://github.com/evgenyrodionov/redux-logger
+import thunkMiddleware from "redux-thunk"; // https://github.com/gaearon/redux-thunk
 
-//IMPORT REDUCERS HERE
-
-const rootReducer = combineReducers({
+const RootReducer = combineReducers({
+  members: MembersReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
-
-export default store;
+export default createStore(
+  RootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      // `withExtraArgument` gives us access to axios in our async action creators!
+      // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
+      thunkMiddleware.withExtraArgument({ axios }),
+      loggingMiddleware
+    )
+  )
+);
