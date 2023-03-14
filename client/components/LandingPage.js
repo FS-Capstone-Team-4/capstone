0720f34bdb1b66eaa79640f31df5b80c9d19bfce
Aidan Denahy <denahyaidan@gmail.com>
2023-03-14 18:50:21 -0400

    storeUpdated

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Map from './Map';
 import Navbar from './Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { loadMembers } from '../store/MembersReducer';
 
 const LandingPage = () => {
+
+  const state = useSelector(state => state);
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    dispatch(loadMembers())
+  }, [])
+
+
   return (
     <div>
       <h1>Landing Page</h1>
--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,6 @@ import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import axios from "axios";
 import MembersReducer from "./MembersReducer";
-import StudentsReducer from "./StudentsReducer";
 import loggingMiddleware from "redux-logger"; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from "redux-thunk"; // https://github.com/gaearon/redux-thunk
 
@@ -15,8 +14,6 @@ export default createStore(
   RootReducer,
   composeWithDevTools(
     applyMiddleware(
-      // `withExtraArgument` gives us access to axios in our async action creators!
-      // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
       thunkMiddleware.withExtraArgument({ axios }),
       loggingMiddleware
     )
