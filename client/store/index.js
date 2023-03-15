afcbde6cfeb6bd3c67af7787d000e446838085e2
Aidan Denahy <denahyaidan@gmail.com>
2023-03-15 18:48:46 -0400

    storage

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,6 @@ import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import axios from "axios";
 import MembersReducer from "./MembersReducer";
-import StudentsReducer from "./StudentsReducer";
 import loggingMiddleware from "redux-logger"; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from "redux-thunk"; // https://github.com/gaearon/redux-thunk
 
--- server/db/conn.js
+++ server/db/conn.js
@@ -5,6 +5,6 @@ const config = {
 if(process.env.QUIET){
   config.logging = false;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/polifacts_db', config);
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5431/polifacts_db', config);
 
 module.exports = conn;
