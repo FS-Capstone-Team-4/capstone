d111a6bd75e39228e12493ae05648d077a1a6fe0
Irwing Alvarez <irwingalvarez@gmail.com>
2023-03-17 16:22:56 -0500

    merged map centering with main

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
-const config = {
-};
+const config = {};
 
-if(process.env.QUIET){
+if (process.env.QUIET) {
   config.logging = false;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5431/polifacts_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/polifacts_db',
+  config
+);
 
 module.exports = conn;
