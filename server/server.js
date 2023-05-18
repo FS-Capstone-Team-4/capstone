d9b59b7931dd2413f6672eccf318671c961d1ef4
Aidan Denahy <denahyaidan@gmail.com>
2023-05-18 12:31:15 -0400

    publicChange

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', require('./api'))
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
-app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
