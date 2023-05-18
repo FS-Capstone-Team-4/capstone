df57ba50a75366cf43a3aea918fdfc0ef5979669
Aidan Denahy <denahyaidan@gmail.com>
2023-05-18 12:18:31 -0400

    1011Change

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 
 // static middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', require('./api'))
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use("/public", express.static(path.join(__dirname, "../public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
